60ec424ad601cfe2d7bdb35f7a76adb5e55590dd
zanxiong <zanx213@163.com>
2022-04-28 00:08:31 +0800

    修改uploadImage

--- src/components/UploadImage/index.vue
+++ src/components/UploadImage/index.vue
@@ -2,7 +2,6 @@
 import { ref, computed } from 'vue'
 import { PlusOutlined } from '@ant-design/icons-vue'
 import { message } from 'ant-design-vue'
-import { deepClone } from '@/utils'
 import common from '@/request/api/common'
 const props = defineProps({
   list: Array,
@@ -39,11 +38,11 @@ const fileList = computed({
 
 // 点击预览
 const handlePreview = async file => {
-  // if (!file.url && !file.preview) {
-  //   file.preview = await getBase64(file.originFileObj)
-  // }
+  if (!file.url && !file.preview) {
+    file.preview = await getBase64(file.originFileObj)
+  }
 
-  previewImage.value = file.url || file.preview || file.thumbUrl
+  previewImage.value = file.url || file.preview
   previewVisible.value = true
 }
 
@@ -60,76 +59,14 @@ const getBase64 = file => {
 
 // 上传之前
 const beforeUpload = (file, list) => {
-  // return new Promise((resolve, reject) => {
-  //   const max = maxCount.value
-  //   if (max <= fileList.length) {
-  //     message.error(`最多上传${max}张图片`)
-  //     return reject()
-  //   }
-  //   return resolve()
-  // })
-
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
+    const afterMax = list.length + fileList.value.length
     const max = maxCount.value
-    if (max <= fileList.length) {
+    if (max <= afterMax) {
       message.error(`最多上传${max}张图片`)
       return reject()
     }
-    // 图片压缩
-    let reader = new FileReader(),
-      img = new Image()
-    reader.readAsDataURL(file)
-    reader.onload = function (e) {
-      img.src = e.target.result
-    }
-
-    img.onload = function () {
-      let canvas = document.createElement('canvas')
-      let context = canvas.getContext('2d')
-
-      let originWidth = this.width
-      let originHeight = this.height
-
-      canvas.width = originWidth
-      canvas.height = originHeight
-
-      context.clearRect(0, 0, originWidth, originHeight)
-      context.drawImage(img, 0, 0, originWidth, originHeight)
-
-      canvas.toBlob(
-        blob => {
-          let imgFile = new File([blob], file.name, { type: file.type }) // 将blob对象转化为图片文件
-          resolve(imgFile)
-        },
-        file.type,
-        0.2
-      ) // file压缩的图片类型
-    }
-  })
-}
-
-// 自定义的上传请求
-const customImageRequest = info => {
-  const { file } = info
-  // 组装数据
-  const params = new FormData()
-  params.append('image', file)
-  const config = {
-    headers: {
-      'Content-Type': 'multipart/form-data'
-    }
-  }
-  const oldList = deepClone(fileList.value)
-  // 发送请求
-  common.uploadImage2(params, config).then(res => {
-    const item = deepClone(fileList.value[fileList.value.length - 1])
-    if (item) {
-      item.status = 'done'
-      item.response = { data: res.data }
-      item.url = `http://81.70.234.149:9002/${res.data}`
-      item.uid = res.data
-    }
-    fileList.value = [...oldList, item]
+    return resolve()
   })
 }
 </script>
@@ -145,7 +82,6 @@ const customImageRequest = info => {
     :action="action"
     :headers="uploadHeaders"
     :before-upload="beforeUpload"
-    :customRequest="customImageRequest"
     @preview="handlePreview"
   >
     <div v-if="fileList.length < maxCount">
